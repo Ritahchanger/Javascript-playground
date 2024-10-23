e68e8882188a347852f24c0a9dd9f62c8e6b68a6
Dennis Peter <peterdennis573@gmail.com>
2024-10-23 14:22:32 +0300

    Added navbar component

--- modernlandingpage/src/App.js
+++ modernlandingpage/src/App.js
@@ -1,7 +1,18 @@
+import Navbar from "./components/Navbar";
+
+import HeroSection from "./components/HeroSection";
+
+
+
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <>
+      <Navbar />
+      <div className="max-w-7xl mx-auto pt-20 px-6">
+        <HeroSection/>
+      </div>
+    </>
+  );
+};
 
-export default App
+export default App;
