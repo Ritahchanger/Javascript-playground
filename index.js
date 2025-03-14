79c8c7b6b826afad688252fc9799bb01de4678b0
Dennis Peter <peterdennis573@gmail.com>
2025-03-14 09:44:38 +0300

    Updates

--- index.js
+++ index.js
@@ -1,39 +1,57 @@
-class Animal{
+function isPalindrome(str){
 
-    makeSound(){
+    let reversed = str.split('').reverse().join('');
 
-        console.log("Animal makes sound");
+    return str === reversed;
+
+}
+
+console.log(isPalindrome("racecar"))
+console.log(isPalindrome("hello"))
+
+function countVowels(str){
+
+    let vowels = "aeiouAEIOU";
+
+    let count = 0;
+
+    for(let car of str){
+
+        if(vowels.includes(car)){
+            count++;
+        }
+
+    }
+
+    return count;
+
+}
+
+console.log(countVowels("hello, world"));
+
+function howLongArevowelsRepeated(str){
+
+    let vowels = "aeiouAEIOU";
+
+    if(str.length === 0) return -1;
+
+    const map = new Map();
+
+    for(let car of str){
+
+        if(vowels.includes(car)){
+
+            map.set(car, map.has(car) ? map.get(car) + 1 : 1 )
+
+        }
+
+    }
+
+    for(let [vowel,count] of map){
+
+        console.log(`${vowel} : ${count}`)
 
     }
 
 }
-
-
-class Dog extends Animal{
-
-    makeSound(){
-
-        console.log("Woof! Woof!");
-
-    }
-
-}
-
-class Cat extends Animal{
-
-    makeSound(){
-
-        console.log("Meow!");
-
-    }
-
-}
-
-const dog = new Dog();
-
-dog.makeSound();
-
-
-const cat = new Cat();
-
-cat.makeSound()
+howLongArevowelsRepeated("hello world, how are you?")
